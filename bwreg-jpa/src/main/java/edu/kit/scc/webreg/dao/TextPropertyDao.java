fef0c2b7390a7ceb5d1c937827fbe16af04c988a
Michael Simon <simon@kit.edu>
2015-03-27 08:23:02 +0100

    using guava cache for db message properties. added daos for message properties.

---
+++ bwreg-jpa/src/main/java/edu/kit/scc/webreg/dao/TextPropertyDao.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Michael Simon.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Michael Simon - initial
+ ******************************************************************************/
+package edu.kit.scc.webreg.dao;
+
+import edu.kit.scc.webreg.entity.TextPropertyEntity;
+
+public interface TextPropertyDao extends BaseDao<TextPropertyEntity, Long> {
+
+	TextPropertyEntity findByKeyAndLang(String key, String language);
+
+}
--- bwreg-webapp/src/main/java/edu/kit/scc/webreg/res/DbMessageBundle.java
+++ bwreg-webapp/src/main/java/edu/kit/scc/webreg/res/DbMessageBundle.java
@@ -2,33 +2,67 @@ package edu.kit.scc.webreg.res;
 
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.faces.context.FacesContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import edu.kit.scc.webreg.dao.TextPropertyDao;
+import edu.kit.scc.webreg.entity.TextPropertyEntity;
+import edu.kit.scc.webreg.util.BeanHelper;
+
 public class DbMessageBundle extends ResourceBundle {
 
+	public static final Logger logger = LoggerFactory.getLogger(DbMessageBundle.class);
 	public static final String BASE_NAME = "msg.messages";
 	
-	private Map<String, String> messages;
+	private LoadingCache<String, BundleEntry> messages;
+	
+	private Locale locale;
+	
+	private TextPropertyDao textPropertyDao;
 	
 	public DbMessageBundle() {
-		Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
 		ResourceBundle rb = ResourceBundle.getBundle(BASE_NAME, locale);
 		setParent(rb);
 		
-		messages = new HashMap<String, String>();
-		messages.put("server_config", "Server Configuration");
+		textPropertyDao = BeanHelper.findBean("jpaTextPropertyDao");
+		
+		messages = CacheBuilder.newBuilder()
+				.concurrencyLevel(4)
+				.weakKeys()
+				.maximumSize(10000)
+				.expireAfterWrite(30, TimeUnit.SECONDS)
+				.build(new CacheLoader<String, BundleEntry>() {
+					public BundleEntry load(String key) {
+						BundleEntry be = new BundleEntry();
+						TextPropertyEntity tpe = textPropertyDao
+								.findByKeyAndLang(key, locale.getLanguage());
+						if (tpe != null)
+							be.setValue(tpe.getValue());
+						return be;
+					}
+				});
 	}
 
     @Override
     protected Object handleGetObject(String key) {
     	Object o = null;
+
     	if (messages != null) {
-    		o = messages.get(key);
+    		BundleEntry be = messages.getUnchecked(key);
+    		if (be != null)
+    			o = be.getValue();
     	}
 
     	if (o == null) {
@@ -40,6 +74,8 @@ public class DbMessageBundle extends ResourceBundle {
 
     @Override
     public Enumeration<String> getKeys() {
-        return messages != null ? Collections.enumeration(messages.keySet()) : parent.getKeys();
+    	Set<String> rets = messages.asMap().keySet();
+    	rets.addAll(parent.keySet());
+        return Collections.enumeration(rets);
     }
 }
