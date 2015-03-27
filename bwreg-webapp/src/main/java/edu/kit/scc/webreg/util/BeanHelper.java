78b59af76263b43f3fe839833b497845959eedfc
Michael Simon <simon@kit.edu>
2015-03-27 08:22:35 +0100

    Guava deps added for cache map

---
+++ bwreg-webapp/src/main/java/edu/kit/scc/webreg/util/BeanHelper.java
@@ -0,0 +1,12 @@
+package edu.kit.scc.webreg.util;
+
+import javax.faces.context.FacesContext;
+
+public class BeanHelper {
+
+	@SuppressWarnings("unchecked")
+	public static <T> T findBean(String beanName) {
+	    FacesContext context = FacesContext.getCurrentInstance();
+	    return (T) context.getApplication().evaluateExpressionGet(context, "#{" + beanName + "}", Object.class);
+	}
+}
