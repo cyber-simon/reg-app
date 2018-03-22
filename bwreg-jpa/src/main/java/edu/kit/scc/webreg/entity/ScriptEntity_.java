b80cc3135c7d4d993e4e63383951afc1ba3f799a
Michael Simon <simon@kit.edu>
2018-03-22 07:32:52 +0100

    add entity classes

---
+++ bwreg-jpa/src/main/java/edu/kit/scc/webreg/entity/ScriptEntity_.java
@@ -0,0 +1,15 @@
+package edu.kit.scc.webreg.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(ScriptEntity.class)
+public abstract class ScriptEntity_ extends edu.kit.scc.webreg.entity.AbstractBaseEntity_ {
+
+	public static volatile SingularAttribute<ScriptEntity, String> script;
+	public static volatile SingularAttribute<ScriptEntity, String> scriptType;
+
+}
+
