63bfc54c33354361c2090968f88a43fa0ec64bf0
Michael Simon <simon@kit.edu>
2018-04-20 09:16:44 +0200

    first user successfully created

--- bwreg-service/src/main/java/edu/kit/scc/webreg/dto/service/ExternalUserDtoServiceImpl.java
+++ bwreg-service/src/main/java/edu/kit/scc/webreg/dto/service/ExternalUserDtoServiceImpl.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 
 import edu.kit.scc.webreg.dao.BaseDao;
 import edu.kit.scc.webreg.dao.ExternalUserDao;
+import edu.kit.scc.webreg.dao.SerialDao;
 import edu.kit.scc.webreg.dto.entity.ExternalUserEntityDto;
 import edu.kit.scc.webreg.dto.mapper.BaseEntityMapper;
 import edu.kit.scc.webreg.dto.mapper.ExternalUserEntityMapper;
@@ -27,6 +28,9 @@ public class ExternalUserDtoServiceImpl extends BaseDtoServiceImpl<ExternalUserE
 	@Inject
 	private ExternalUserDao dao;
 
+	@Inject
+	private SerialDao serialDao;
+	
 	@Override
 	public ExternalUserEntityDto findByExternalId(String externalId) throws NoUserFoundException {
 		ExternalUserEntity entity = dao.findByExternalId(externalId);
@@ -44,6 +48,7 @@ public class ExternalUserDtoServiceImpl extends BaseDtoServiceImpl<ExternalUserE
 			throw new UserCreateException("user already exists");
 		entity = dao.createNew();
 		reverseMapper.copyProperties(dto, entity);
+		entity.setUidNumber(serialDao.next("uid-number-serial").intValue());
 		entity = dao.persist(entity);
 	}
 	
---
+++ bwreg-webapp/src/main/java/edu/kit/scc/webreg/rest/exc/UserCreateExceptionMapper.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Michael Simon.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Michael Simon - initial
+ ******************************************************************************/
+package edu.kit.scc.webreg.rest.exc;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import edu.kit.scc.webreg.exc.UserCreateException;
+
+@Provider
+public class UserCreateExceptionMapper implements ExceptionMapper<UserCreateException> {
+
+	@Override
+	public Response toResponse(UserCreateException ex) {
+		return Response.status(405).entity(ex.getMessage())
+				.type(MediaType.TEXT_PLAIN).build();
+	}
+
+}
