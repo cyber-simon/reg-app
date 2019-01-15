a2335f6fe6e9b900194bf88bf768604b0ccabf48
Michael Simon <simon@kit.edu>
2019-01-15 14:32:35 +0100

    add page for viewing and deploying keys, no function yet.

---
+++ bwreg-webapp/src/main/java/edu/kit/scc/webreg/bean/UserSshKeyManagementBean.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Michael Simon.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Michael Simon - initial
+ ******************************************************************************/
+package edu.kit.scc.webreg.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Inject;
+
+import edu.kit.scc.webreg.entity.UserEntity;
+import edu.kit.scc.webreg.service.UserService;
+import edu.kit.scc.webreg.session.SessionManager;
+import edu.kit.scc.webreg.ssh.OpenSshKeyDecoder;
+import edu.kit.scc.webreg.ssh.OpenSshPublicKey;
+
+@ManagedBean
+@ViewScoped
+public class UserSshKeyManagementBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private UserEntity user;
+	
+	@Inject
+	private UserService userService;
+	
+    @Inject 
+    private SessionManager sessionManager;
+
+    @Inject
+    private OpenSshKeyDecoder keyDecoder;
+    
+    private List<OpenSshPublicKey> keyList;
+    private String newKey;
+    
+	public void preRenderView(ComponentSystemEvent ev) {
+		if (user == null) {
+	    	user = userService.findByIdWithStore(sessionManager.getUserId());
+	    	keyList = new ArrayList<>();
+		}
+	}
+	
+	public void deployKey() {
+		OpenSshPublicKey key = keyDecoder.decode(newKey);
+		keyList.add(key);
+	}
+	
+	public UserEntity getUser() {
+		return user;
+	}
+
+	public String getNewKey() {
+		return newKey;
+	}
+
+	public void setNewKey(String newKey) {
+		this.newKey = newKey;
+	}
+
+	public List<OpenSshPublicKey> getKeyList() {
+		return keyList;
+	}
+
+	public void setKeyList(List<OpenSshPublicKey> keyList) {
+		this.keyList = keyList;
+	}
+}
