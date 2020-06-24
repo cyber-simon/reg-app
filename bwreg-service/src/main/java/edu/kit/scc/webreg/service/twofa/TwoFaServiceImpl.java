2167c0009330a959cde60f1805b3a0036f7fd572
ls1947 <ls1947@scctest-idp-dev-02.scc.kit.edu>
2020-06-24 15:16:19 +0200

    first list existing tokens

--- bwreg-service/src/main/java/edu/kit/scc/webreg/service/twofa/TwoFaServiceImpl.java
+++ bwreg-service/src/main/java/edu/kit/scc/webreg/service/twofa/TwoFaServiceImpl.java
@@ -16,12 +16,14 @@ import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthCache;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
@@ -30,6 +32,8 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.kit.scc.webreg.dao.UserDao;
 import edu.kit.scc.webreg.entity.UserEntity;
@@ -71,30 +75,70 @@ public class TwoFaServiceImpl implements TwoFaService {
 				    .setConnectTimeout(5000)
 				    .build();
 			CloseableHttpClient httpClient = HttpClients.custom().setDefaultRequestConfig(config).build();
-
+			String adminSession = null;
+			
 			try {
 				
 				HttpPost httpPost = new HttpPost(configMap.get("url") + "/admin/getsession");
 
-		        CloseableHttpResponse response = httpClient.execute(httpPost, context);
+		        CloseableHttpResponse response = httpClient.execute(targetHost, httpPost, context);
 				try {
 			        HttpEntity entity = response.getEntity();
 		            String responseString = EntityUtils.toString(entity);
 		            logger.debug(responseString);
+		            
+		            CookieStore cookieStore = context.getCookieStore();
+		            List<Cookie> cookies = cookieStore.getCookies();
+		            for (Cookie cookie : cookies) {
+		            	logger.debug("Cookie {}: {}", cookie.getName(), cookie.getValue());
+		            	if (cookie.getName().equalsIgnoreCase("admin_session")) {
+		            		adminSession = cookie.getValue();
+		            	}
+		            }
 				} finally {
 					response.close();
+				}
+				
+				if (adminSession == null) {
+					throw new TwoFaException("LinOTP issued no admin session. Cannot continue.");
 				}
 
 				httpPost = new HttpPost(configMap.get("url") + "/admin/show");
 				List<NameValuePair> nvps = new ArrayList <NameValuePair>();
-		        nvps.add(new BasicNameValuePair("user", "test@kit.edu"));
+		        if (configMap.containsKey("userId"))
+			        nvps.add(new BasicNameValuePair("user", configMap.get("userId")));
+		        else
+		        	nvps.add(new BasicNameValuePair("user", user.getEppn()));
+		        if (configMap.containsKey("realm"))
+		        	nvps.add(new BasicNameValuePair("realm", configMap.get("realm")));
+		        nvps.add(new BasicNameValuePair("session", adminSession));
 		        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
 		        
-		        response = httpClient.execute(httpPost);
+		        response = httpClient.execute(targetHost, httpPost, context);
 				try {
 			        HttpEntity entity = response.getEntity();
 		            String responseString = EntityUtils.toString(entity);
 		            logger.debug(responseString);
+		            ObjectMapper om = new ObjectMapper();
+					@SuppressWarnings("unchecked")
+					Map<String, Object> map = om.readValue(responseString, Map.class);
+					if (map.get("result") instanceof Map<?, ?>) {
+						Map<?, ?> resultMap = (Map<?, ?>) map.get("result");
+			            logger.debug("value: " + resultMap.get("value").getClass().toString());
+						if (resultMap.get("value") instanceof Map<?, ?>) {
+							Map<?, ?> valueMap = (Map<?, ?>) resultMap.get("value");
+				            logger.debug("data: " + valueMap.get("data").getClass().toString());
+				            if (valueMap.get("data") instanceof List<?>) {
+				            	List<?> dataList = (List<?>) valueMap.get("data");
+				            	for (Object data : dataList) {
+				            		if (data instanceof Map<?, ?>) {
+				            			Map<?, ?> dataMap = (Map<?, ?>) data;
+					            		logger.debug("ID: " + dataMap.get("LinOtp.TokenId"));
+				            		}
+				            	}
+				            }
+						}
+					}
 				} finally {
 					response.close();
 				}
