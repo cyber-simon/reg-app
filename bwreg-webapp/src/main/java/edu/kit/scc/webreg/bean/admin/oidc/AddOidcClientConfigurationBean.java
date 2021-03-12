cd28d586055a12a5803d55869cd05ff026056913
Michael Simon <simon@kit.edu>
2021-03-12 07:55:22 +0100

    add oidc client configuration pages

---
+++ bwreg-webapp/src/main/java/edu/kit/scc/webreg/bean/admin/oidc/AddOidcClientConfigurationBean.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Michael Simon.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Michael Simon - initial
+ ******************************************************************************/
+package edu.kit.scc.webreg.bean.admin.oidc;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import edu.kit.scc.webreg.entity.oidc.OidcClientConfigurationEntity;
+import edu.kit.scc.webreg.service.oidc.OidcClientConfigurationService;
+
+@Named("addOidcClientConfigurationBean")
+@RequestScoped
+public class AddOidcClientConfigurationBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private OidcClientConfigurationService service;
+
+	private OidcClientConfigurationEntity entity;
+	
+	@PostConstruct
+	public void init() {
+		entity = service.createNew();
+	}
+
+	public String save() {
+		entity = service.save(entity);
+		return "show-client-config.xhtml?faces-redirect=true&id=" + entity.getId();
+	}
+	
+	public OidcClientConfigurationEntity getEntity() {
+		return entity;
+	}
+
+	public void setEntity(OidcClientConfigurationEntity entity) {
+		this.entity = entity;
+	}
+}
---
+++ bwreg-webapp/src/main/java/edu/kit/scc/webreg/bean/admin/oidc/EditOidcClientConfigurationBean.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Michael Simon.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Michael Simon - initial
+ ******************************************************************************/
+package edu.kit.scc.webreg.bean.admin.oidc;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Inject;
+
+import edu.kit.scc.webreg.entity.oidc.OidcClientConfigurationEntity;
+import edu.kit.scc.webreg.service.oidc.OidcClientConfigurationService;
+
+@ManagedBean
+@ViewScoped
+public class EditOidcClientConfigurationBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private OidcClientConfigurationService service;
+	
+	private OidcClientConfigurationEntity entity;
+	
+	private Long id;
+	
+	public void preRenderView(ComponentSystemEvent ev) {
+		if (entity == null) {
+			entity = service.findById(id);
+		}
+	}
+	
+	public String save() {
+		service.save(entity);
+		return "show-client-config.xhtml?faces-redirect=true&id=" + entity.getId();
+	}
+
+	public OidcClientConfigurationEntity getEntity() {
+		return entity;
+	}
+
+	public void setEntity(OidcClientConfigurationEntity entity) {
+		this.entity = entity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
